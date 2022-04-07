8a070d36c0a8001680ee9027ec8847117394f8ca
parkeunji <jjjj584@doubled.kr>
2022-04-07 22:55:01 +0900

    asset 추가

--- app/src/main/java/com/eundmswlji/tacoling/MainApplication.kt
+++ app/src/main/java/com/eundmswlji/tacoling/MainApplication.kt
@@ -1,9 +1,13 @@
 package com.eundmswlji.tacoling
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MainApplication:Application()  {
-
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
 }
