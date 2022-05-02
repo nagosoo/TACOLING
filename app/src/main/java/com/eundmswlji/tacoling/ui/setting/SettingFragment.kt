ad3c88e4c61187941be4bbc4356a3a6b5be2e135
EunJiPark <jjjj584@doubled.kr>
2022-05-02 16:14:31 +0900

    settingFragment 진행중

--- app/src/main/java/com/eundmswlji/tacoling/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/eundmswlji/tacoling/ui/setting/SettingFragment.kt
@@ -1,16 +1,19 @@
 package com.eundmswlji.tacoling.ui.setting
 
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import androidx.paging.cachedIn
+import com.eundmswlji.tacoling.BuildConfig
 import com.eundmswlji.tacoling.data.repository.JusoRepository
 import com.eundmswlji.tacoling.databinding.FragmentSettingBinding
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class SettingFragment : Fragment() {
@@ -24,5 +27,34 @@ class SettingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setAppBar()
+        setOnClickListener()
+    }
+
+    private fun setAppBar() {
+        binding.appBar.tv.text = "마이페이지"
+        binding.logout.tv.text = "로그아웃"
+        binding.alarm.tv.text = "알림 설정"
+        binding.myZzim.tv.text = "내가 찜한 가게"
+        binding.suggest.tv.text = "건의하기"
+        binding.withdrawal.tv.text = "탈퇴하기"
+    }
+
+    private fun setOnClickListener() {
+        binding.suggest.root.setOnClickListener {
+            sendEmailToAdmin()
+        }
+    }
+
+    private fun sendEmailToAdmin() {
+        val email = Intent(Intent.ACTION_SENDTO)
+        email.putExtra(Intent.EXTRA_SUBJECT, "건의하기")
+        email.putExtra(Intent.EXTRA_EMAIL, "nagosoo@kakao.com")
+        email.putExtra(
+            Intent.EXTRA_TEXT,
+            "App Version : ${BuildConfig.VERSION_NAME}\nDevice : ${Build.DEVICE}\nAndroid(SDK) : ${Build.VERSION.SDK_INT}(${Build.VERSION.RELEASE})\n내용 : ",
+        )
+        email.type = "message/rfc822"
+        requireContext().startActivity(email)
     }
 }
