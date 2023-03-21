8843c3a07956659362d3498da704898d43d549f2
parkeunji <nagosoo@kakao.com>
2023-03-22 00:24:28 +0900

    fcm 붙임

---
+++ app/src/main/java/com/eundmswlji/tacoling/fcm/MyFireBaseMessagingService.kt
@@ -0,0 +1,124 @@
+package com.eundmswlji.tacoling.fcm
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.eundmswlji.tacoling.R
+import com.eundmswlji.tacoling.ui.MainActivity
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import kotlin.random.Random
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    private val TAG = "LOGGING"
+
+    //백그라운드에서 푸시 알림을 받을때 포그라운드와 동일한 처리를 해주고 싶으면 handleIntent를 override 해야함
+    override fun handleIntent(intent: Intent) {
+        try {
+            if (intent.extras != null) {
+                val builder = RemoteMessage.Builder("MyFirebaseMessagingService")
+                for (key in intent.extras!!.keySet()) {
+                    print("key: $key")
+                    builder.addData(key!!, intent.extras!!.getString(key))
+                }
+                onMessageReceived(builder.build())
+            } else {
+                super.handleIntent(intent)
+            }
+        } catch (e: Exception) {
+            super.handleIntent(intent)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        Log.d("fcmTest", "${message.notification?.title}")
+        Log.d("fcmTest", "${message.notification?.body}")
+        Log.d("fcmTest", "${message.data["shopId"]}")
+        createNotificationChannel() //채널 만들기
+        message.notification?.let {
+            with(NotificationManagerCompat.from(this)) {
+                notify(
+                    Random.nextInt(),
+                    createNotificationNormal(it.title ?: "알림", it.body ?: "알림입니다.", message.data)
+                )
+            }
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d(TAG, "Refreshed token: $token")
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_id)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val id = getString(R.string.channel_id)
+            val channel = NotificationChannel(id, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun createNotificationNormal(
+        title: String,
+        content: String,
+        data: Map<String, String>?
+    ): Notification {
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            if (!data.isNullOrEmpty()) {
+                val args = Bundle()
+                args.putString("shopId", data["shopId"])
+                putExtra("bundle", args)
+            }
+            flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        }
+        val pendingIntent =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                PendingIntent.getActivity(
+                    this,
+                    100,
+                    intent,
+                    PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+                )
+            } else {
+                PendingIntent.getActivity(
+                    this,
+                    100,
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+            }
+
+        return NotificationCompat.Builder(this, getString(R.string.channel_id))
+            .setSmallIcon(R.drawable.ic_taco)
+            .setContentTitle(title)
+            .setContentText(content)
+            .setAutoCancel(true)
+            .setChannelId(getString(R.string.channel_id))
+            .setContentIntent(pendingIntent)
+            .build()
+    }
+
+}
--- app/src/main/java/com/eundmswlji/tacoling/ui/MainActivity.kt
+++ app/src/main/java/com/eundmswlji/tacoling/ui/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.eundmswlji.tacoling.ui
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
@@ -12,6 +18,8 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.eundmswlji.tacoling.R
 import com.eundmswlji.tacoling.databinding.ActivityMainBinding
+import com.eundmswlji.tacoling.ui.dialog.NormalDialog
+import com.eundmswlji.tacoling.util.Util.toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 import com.google.firebase.dynamiclinks.ktx.dynamicLinks
@@ -40,9 +48,50 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
         binding.bottomNavView.setOnItemSelectedListener(this)
         binding.bottomNavView.setOnItemReselectedListener(this)
 
+        askNotificationPermission()
         observer()
         viewModel.getUserId()
+    }
 
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        intent?.let {
+            val shopId = it.getBundleExtra("bundle")?.getString("shopId")
+            shopId?.let {
+                navController.navigate(R.id.shopFragment, Bundle().apply {
+                    putInt("shopId", shopId.toInt())
+                })
+            }
+            it.removeExtra("shopId")
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+        } else {
+            toast("알림을 받기 위해서는 알림 권한을 허용해야 합니다.")
+        }
+    }
+
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                NormalDialog("알림 권한",
+                    "알림을 받기 위해서는 알림 권한을 허용해야 합니다.",
+                    positiveMessage = "확인",
+                    negativeMessage = "취소",
+                    positiveButtonListener = {
+                        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                    }).show(supportFragmentManager, null)
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
     }
 
     private fun observer() {
@@ -54,13 +103,27 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
 
     private fun setNavGraph(doneLogin: Boolean) {
         val navGraph =
-            navController.navInflater.inflate(R.navigation.nav_graph) // app:navGraph="@navigation/nav_graph" 로 설정했던 것
+            navController.navInflater.inflate(R.navigation.nav_graph)
         if (doneLogin) {
             navGraph.setStartDestination(R.id.mapFragment)
             getDynamicLink()
+            navController.setGraph(navGraph, null) //navController에 graph 설정
+            manageBackgroundNotification()
         } //setStartDestination 설정
-        else navGraph.setStartDestination(R.id.loginFragment)
-        navController.setGraph(navGraph, null) //navController에 graph 설정
+        else {
+            navGraph.setStartDestination(R.id.loginFragment)
+            navController.setGraph(navGraph, null) //navController에 graph 설정
+        }
+    }
+
+    private fun manageBackgroundNotification() {
+        val shopId = intent.getBundleExtra("bundle")?.getString("shopId")
+        shopId?.let {
+            intent.removeExtra("shopId")
+            navController.navigate(R.id.shopFragment, Bundle().apply {
+                putInt("shopId", shopId.toInt())
+            })
+        }
     }
 
 //    override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
